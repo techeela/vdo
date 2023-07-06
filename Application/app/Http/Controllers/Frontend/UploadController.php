6b3d424d0f205f8ffd5183a1e4c3b50c5b887ceb
Top Coder <ottsnapteam@gmail.com>
2023-07-07 01:59:47 +0530

    trans

--- Application/app/Http/Controllers/Frontend/UploadController.php
+++ Application/app/Http/Controllers/Frontend/UploadController.php
@@ -135,8 +135,9 @@ class UploadController extends Controller
                     'extension' => $fileExtension,
                     'filename' => $uploadResponse->filename,
                     'path' => $uploadResponse->path,
-                    'cdn_url' => $cdn_url,
-                    'link' => $uploadResponse->link,
+                    //'cdn_url' => $cdn_url,
+                    //'link' => $uploadResponse->link,
+                    'link' => $cdn_url,
                     'password' => $request->password,
                     'expiry_at' => $expiryAt,
                 ]);
