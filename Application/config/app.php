925ad12ad84c72090c3e8854cb4911bf7cd295ae
Kaafal Admin <kaafaltech@gmail.com>
2023-07-06 07:25:19 +0000

    changed

--- Application/config/app.php
+++ Application/config/app.php
@@ -54,7 +54,7 @@ return [
     |
      */
 
-    'url' => env('APP_URL', 'https://console.ottanalytics.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
