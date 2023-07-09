19eb6bc295829edee11b077030242204edef546b
Top Coder <ottsnapteam@gmail.com>
2023-07-09 10:15:24 +0530

    trans

--- Application/resources/views/frontend/file/embed.blade.php
+++ Application/resources/views/frontend/file/embed.blade.php
@@ -1,6 +1,6 @@
 
 
-<div style="position: relative; padding-top: 56.25%;">
+<div style="position: relative; ">
   <iframe src="https://iframe.mediadelivery.net/embed/135513/{{ $fileEntry->bunny_id }}?autoplay=true" 
       loading="lazy" style="border: none; position: absolute; top: 0; height: 100%; width: 100%;" 
       allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true">
