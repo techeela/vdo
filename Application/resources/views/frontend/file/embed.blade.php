bd801a1fbc7d25caa1c3ca8703cd972673b4b60b
Top Coder <ottsnapteam@gmail.com>
2023-07-09 09:59:33 +0530

    trans

--- Application/resources/views/frontend/file/embed.blade.php
+++ Application/resources/views/frontend/file/embed.blade.php
@@ -1,1 +1,84 @@
-<!DOCTYPE html> <html lang="{{ getLang() }}"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>{{ pageTitle($__env) }}</title> <link rel="icon" href="{{ asset($settings['website_favicon']) }}" /> <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap/bootstrap.min.css') }}"> <link rel="stylesheet" href="{{ asset('assets/vendor/libs/plyr/plyr.min.css') }}" /> <link rel="stylesheet" href="{{ asset(mix('assets/css/application.css')) }}"> </head> <body class="pt-0"> <div class="fileviewer-embed"><video controls playsinline id="video"></video></div> </body> <script src="{{ asset('assets/vendor/libs/jquery/jquery.min.js') }}"></script> <script src="{{ asset('assets/vendor/libs/bootstrap/bootstrap.bundle.min.js') }}"></script> <script src="{{ asset('assets/vendor/libs/plyr/plyr.min.js') }}"></script> <script> let video = document.querySelector("#video"); if (video) { const player = new Plyr(video); player.source = { type: 'video', title: "{{ $fileEntry->name }}", sources: [{ src: "{{ $fileEntry->link }}", type: "{{ $fileEntry->mime }}", }], }; } </script> </html>
+
+
+
+
+
+<div class="container">
+	<video controls crossorigin playsinline poster="https://bitdash-a.akamaihd.net/content/sintel/poster.png"></video>
+</div>
+<!-- Plyr resources and browser polyfills are specified in the pen settings -->
+<!-- Hls.js 0.9.x and 0.10.x both have critical bugs affecting this demo. Using fixed git hash to when it was working (0.10.0 pre-release), until https://github.com/video-dev/hls.js/issues/1790 has been resolved -->
+<script src="https://cdn.rawgit.com/video-dev/hls.js/18bb552/dist/hls.min.js"></script>
+
+
+<script>
+
+document.addEventListener('DOMContentLoaded', () => {
+	const source = {{ $fileEntry->cdn_url }};
+	const video = document.querySelector('video');
+
+	const defaultOptions = {};
+
+	if (!Hls.isSupported()) {
+		video.src = source;
+		var player = new Plyr(video, defaultOptions);
+	} else {
+		// For more Hls.js options, see https://github.com/dailymotion/hls.js
+		const hls = new Hls();
+		hls.loadSource(source);
+
+		// From the m3u8 playlist, hls parses the manifest and returns
+                // all available video qualities. This is important, in this approach,
+    	        // we will have one source on the Plyr player.
+    	       hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+
+	      	     // Transform available levels into an array of integers (height values).
+	      	    const availableQualities = hls.levels.map((l) => l.height)
+	      	availableQualities.unshift(0) //prepend 0 to quality array
+
+	      	    // Add new qualities to option
+		    defaultOptions.quality = {
+		    	default: 0, //Default - AUTO
+		        options: availableQualities,
+		        forced: true,        
+		        onChange: (e) => updateQuality(e),
+		    }
+		    // Add Auto Label 
+		    defaultOptions.i18n = {
+		    	qualityLabel: {
+		    		0: 'Auto',
+		    	},
+		    }
+
+		    hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
+	          var span = document.querySelector(".plyr__menu__container [data-plyr='quality'][value='0'] span")
+	          if (hls.autoLevelEnabled) {
+	            span.innerHTML = `AUTO (${hls.levels[data.level].height}p)`
+	          } else {
+	            span.innerHTML = `AUTO`
+	          }
+	        })
+    
+             // Initialize new Plyr player with quality options
+		     var player = new Plyr(video, defaultOptions);
+         });	
+
+	hls.attachMedia(video);
+    	window.hls = hls;		 
+    }
+
+    function updateQuality(newQuality) {
+      if (newQuality === 0) {
+        window.hls.currentLevel = -1; //Enable AUTO quality if option.value = 0
+      } else {
+        window.hls.levels.forEach((level, levelIndex) => {
+          if (level.height === newQuality) {
+            console.log("Found quality match with " + newQuality);
+            window.hls.currentLevel = levelIndex;
+          }
+        });
+      }
+    }
+});
+
+</script>
