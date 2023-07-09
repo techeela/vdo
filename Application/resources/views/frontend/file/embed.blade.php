eff0708b5fc3b5b0d5a9589c75c48a7b8996f2c0
Top Coder <ottsnapteam@gmail.com>
2023-07-09 10:13:02 +0530

    trans

--- Application/resources/views/frontend/file/embed.blade.php
+++ Application/resources/views/frontend/file/embed.blade.php
@@ -1,1 +1,8 @@
-<!DOCTYPE html> <html lang="{{ getLang() }}"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>{{ pageTitle($__env) }}</title> <link rel="icon" href="{{ asset($settings['website_favicon']) }}" /> <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap/bootstrap.min.css') }}"> <link rel="stylesheet" href="{{ asset('assets/vendor/libs/plyr/plyr.min.css') }}" /> <link rel="stylesheet" href="{{ asset(mix('assets/css/application.css')) }}"> </head> <body class="pt-0"> <div class="fileviewer-embed"><video controls playsinline id="video"></video></div> </body> <script src="{{ asset('assets/vendor/libs/jquery/jquery.min.js') }}"></script> <script src="{{ asset('assets/vendor/libs/bootstrap/bootstrap.bundle.min.js') }}"></script> <script src="{{ asset('assets/vendor/libs/plyr/plyr.min.js') }}"></script> <script> let video = document.querySelector("#video"); if (video) { const player = new Plyr(video); player.source = { type: 'video', title: "{{ $fileEntry->name }}", sources: [{ src: "{{ $fileEntry->link }}", type: "{{ $fileEntry->mime }}", }], }; } </script> </html>
+
+
+<div style="position: relative; padding-top: 56.25%;">
+  <iframe src="https://iframe.mediadelivery.net/embed/135513/{{ $fileEntry->bunny_id }}?autoplay=true" 
+      loading="lazy" style="border: none; position: absolute; top: 0; height: 100%; width: 100%;" 
+      allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;" allowfullscreen="true">
+  </iframe>
+</div>
